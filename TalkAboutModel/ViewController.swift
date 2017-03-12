b33e741a8717f64170a0ac6886fabcd10ec32624
draveness <i@draveness.me>
2017-03-12 12:03:41 +0800

    Initial Commit

---
+++ TalkAboutModel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TalkAboutModel
+//
+//  Created by draveness on 12/03/2017.
+//  Copyright © 2017 draveness. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
